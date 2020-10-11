13f05a048069ffc0a5ce4a7c66faace66fffbb38
Ismael Correa <ismael.correa.25@gmail.com>
2020-10-11 16:52:16 -0300

    AbstractWeaponTest class created, checkconstruction method declared.

--- src/test/java/com/github/cc3002/finalreality/model/weapon/AbstractWeaponTest.java
+++ src/test/java/com/github/cc3002/finalreality/model/weapon/AbstractWeaponTest.java
@@ -1,4 +1,8 @@
 package com.github.cc3002.finalreality.model.weapon;
 
+import com.github.ElficTitious.finalreality.model.weapon.IWeapon;
+
 public abstract class AbstractWeaponTest {
+
+    public abstract void checkConstruction();
 }
