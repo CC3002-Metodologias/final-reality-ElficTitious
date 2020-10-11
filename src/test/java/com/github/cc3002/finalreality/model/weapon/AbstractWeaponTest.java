fbf7f9a54989a7e5f8c2f843d2821b7708c2c46f
Ismael Correa <ismael.correa.25@gmail.com>
2020-10-11 17:09:18 -0300

    AbstractWeaponTest class created, checkconstruction implemented.

--- src/test/java/com/github/cc3002/finalreality/model/weapon/AbstractWeaponTest.java
+++ src/test/java/com/github/cc3002/finalreality/model/weapon/AbstractWeaponTest.java
@@ -1,11 +1,16 @@
 package com.github.cc3002.finalreality.model.weapon;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import com.github.ElficTitious.finalreality.model.weapon.IWeapon;
 
 public abstract class AbstractWeaponTest {
 
     public void checkConstruction(IWeapon expectedWeapon, IWeapon testEqualWeapon,
                                   IWeapon sameClassDifferentWeapon, IWeapon differentClassWeapon) {
-
+        assertEquals(expectedWeapon, expectedWeapon);
+        assertEquals(expectedWeapon, testEqualWeapon);
+        assertNotEquals(expectedWeapon, sameClassDifferentWeapon);
+        assertNotEquals(expectedWeapon, differentClassWeapon);
     }
 }
