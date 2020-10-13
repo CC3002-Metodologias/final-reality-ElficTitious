4f5d0c4148b224b51d12f6dc7f612afd125b4630
Ismael Correa <ismael.correa.25@gmail.com>
2020-10-13 14:46:44 -0300

    testing packages changed.

--- src/test/java/com/github/cc3002/finalreality/model/character/AbstractPlayerCharacterTest.java
+++ src/test/java/com/github/cc3002/finalreality/model/character/AbstractPlayerCharacterTest.java
@@ -1,4 +1,15 @@
 package com.github.cc3002.finalreality.model.character;
 
+import com.github.ElficTitious.finalreality.model.character.ICharacter;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+
 public class AbstractPlayerCharacterTest {
+
+    protected BlockingQueue<ICharacter> turnsQueue;
+    protected ScheduledExecutorService scheduledExecutor;
+
+    void checkConstruction();
+
 }
